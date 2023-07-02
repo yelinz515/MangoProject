831ea761fca08d79e62422172f367a3bbccac7ff
dasl1101 <ektmf1101@gmail.com>
2023-07-02 12:11:42 +0900

    [feat] test2

---
+++ backend/testPro/src/main/java/com/example/testPro/TestProApplication.java
@@ -0,0 +1,13 @@
+package com.example.testPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProApplication.class, args);
+	}
+
+}
